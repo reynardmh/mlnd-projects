4811a9f3e8b2a461b35ade07a0ba60ecd66ebc3e
Reynard Hilman <reynardh@haikulearning.com>
2016-02-07 17:58:26 -0500

    implemented optimal strategy
    failure rate is between 1-2 for 100 iterations.

--- p4-train-smartcab/smartcab/agent.py
+++ p4-train-smartcab/smartcab/agent.py
@@ -3,10 +3,15 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+def debug(s):
+    if False:
+        print s
+
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
     valid_actions = (None, 'forward', 'left', 'right')
-    gamma = 0.8
+    gamma = 0.5
+    exploration_prob = 0.1
 
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
@@ -17,19 +22,28 @@ class LearningAgent(Agent):
         self.last_state = None
         self.last_action = None
         self.last_reward = None
-        self.time = 0
         self.total_reward = 0
+        self.deadline = None
+        # For agent's learning reporting:
+        self.num_iterations = 0
+        self.failure_tracking = {}
+        self.total_penalty = 0
+        self.total_wrong_action = 0
 
     def reset(self, destination=None):
-        print "Total reward: {}".format(self.total_reward)
-        print self.Q_table
-
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
         self.last_state = None
         self.last_action = None
         self.last_reward = None
         self.total_reward = 0
+        self.num_iterations += 1
+
+    def print_Q_table(self):
+        print "--------- Q-table ---------"
+        for s in self.Q_table:
+            print "{}: {}".format(s, self.Q_table[s])
+        print "------- end Q-table -------"
 
     def Q(self, state, action):
         if state in self.Q_table:
@@ -52,16 +66,27 @@ class LearningAgent(Agent):
         for a in self.valid_actions:
             Q_vals[a] = self.Q(self.state, a)
 
-        print self.state
-        print Q_vals
-        # if max(Q_vals.values()) == 0:
-        #     action = random.choice(self.valid_actions)
-        #     # action = self.next_waypoint
-        # else:
-        #     action = max(Q_vals, key=Q_vals.get)
+        # action = self.max_Q_strategy(Q_vals)
+        action = self.optimal_strategy(Q_vals)
 
-        action = self.max_Q_strategy(Q_vals)
-        print "chosen action: {}".format(action)
+        debug("State: {}".format(self.state))
+        debug(Q_vals)
+        debug("chosen action: {}".format(action))
+        return action
+
+    def optimal_strategy(self, Q_vals):
+        action = 'undefined'
+        # Explore new thing.
+        for a, val in Q_vals.items():
+            if val == 0:
+                action = a
+                break
+        if action == 'undefined':
+            if random.random() < self.exploration_prob:
+                # randomly explore non best action just to make sure we really didn't like that action
+                action = random.choice(self.valid_actions)
+            else:
+                action = max(Q_vals, key=Q_vals.get)
         return action
 
     def max_Q_strategy(self, Q_vals):
@@ -70,12 +95,12 @@ class LearningAgent(Agent):
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
-        print "Next waypoint: {}".format(self.next_waypoint)
+        debug("Next waypoint: {}".format(self.next_waypoint))
         inputs = self.env.sense(self)
-        deadline = self.env.get_deadline(self)
+        self.deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        self.state = "{}-{}-{}".format(inputs['light'], inputs['oncoming'], inputs['left']) #, self.next_waypoint , deadline
+        self.state = "{},{}".format(inputs['light'], self.next_waypoint) # inputs['oncoming'], inputs['left'],
 
         # TODO: Select action according to your policy
         action = self.choose_action()
@@ -91,12 +116,27 @@ class LearningAgent(Agent):
         self.last_state = self.state
         self.last_action = action
         self.last_reward = reward
-        self.time += 1
+        if reward < 0:
+            self.total_penalty += 1
+        elif reward < 1:
+            self.total_wrong_action += 1
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-
-        if self.time % 50 == 0:
-            print self.Q_table
+        print("deadline = {}, inputs = {}, direction = {}, action = {}, reward = {}".format(self.deadline, inputs, self.next_waypoint, action, reward))
+        if self.deadline < 0:
+            self.failure_tracking[self.num_iterations - 1] = self.deadline
+        if reward > 2:
+            # Reached destination within deadline
+            print "Total reward: {}".format(self.total_reward)
+            print "Remaining deadline: {}".format(self.deadline)
+            self.print_Q_table()
+            if self.num_iterations == N_TRIALS:
+                print "=== Summary of learning ==="
+                print "Gamma: {}".format(self.gamma)
+                print "Exploration vs Exploitation: {} : {}".format(self.exploration_prob * 10, (1 - self.exploration_prob) * 10)
+                print "Total failures: {} times it did not reach destination within deadline".format(len(self.failure_tracking))
+                print "Total penalties: {} times it got -1".format(self.total_penalty)
+                print "Total wrong action: {} times it did not follow the direction (got 0.5)".format(self.total_wrong_action)
+                print "Iteration where it does not meet deadline: {}".format(self.failure_tracking)
 
 class BasicDrivingAgent(Agent):
     """A basic driving agent"""
@@ -113,18 +153,19 @@ class BasicDrivingAgent(Agent):
         print "BasicDrivingAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
+N_TRIALS = 120
 def run():
     """Run the agent for a finite number of trials."""
 
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
-    # a = e.create_agent(BasicDrivingAgent)  # create agent
+    # a = e.create_agent(BasicDrivingAgent)  # create agent that randomly choose an action
     a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.3)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=10)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=0)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=N_TRIALS)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
